36e08df63f64dd2826ef8ec5afc7eb7d9819a0ad
soydeacon <dpoon@wello.ca>
2024-08-15 11:58:48 -0700

    Add HeroSection component

---
+++ components/sections/HeroSection.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { HeroSection as HeroSectionType } from "@/types";
+
+const HeroSection: React.FC<HeroSectionType> = ({ imageURI }) => {
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{ backgroundImage: `url(${imageURI})` }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hero Section</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
