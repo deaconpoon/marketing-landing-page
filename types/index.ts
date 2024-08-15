b6b20725785b98bc5d8742dee5658668b3296c93
soydeacon <dpoon@wello.ca>
2024-08-15 11:52:09 -0700

    Add basic types in types

---
+++ types/index.ts
@@ -0,0 +1,19 @@
+export interface HeroSection {
+  type: 'hero';
+  imageURI: string;
+}
+
+export interface ImageTextSection {
+  type: 'image-text';
+  imageURI: string;
+  text: string;
+  title?: string;
+  leftToRight?: boolean;
+}
+
+export interface DataSection {
+  type: 'data';
+  url: string;
+}
+
+export type Section = HeroSection | ImageTextSection | DataSection;
